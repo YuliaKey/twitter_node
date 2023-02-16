f7b905f26f2d7411e8a08641ac0f0acf54131785
Yuliia Kolomiiets <jk182013@gmail.com>
2023-02-16 11:40:26 +0100

    feat: creation d'un tweet et affichage de la liste des tweets

---
+++ database/models/Tweet.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema;
+
+const tweetSchema = schema({
+    content: {
+        type: String,
+        maxlength: [146, 'Le tweet est trop long'],
+        minlength: [5, 'Le tweet est trop court'],
+        requires: [ true, "Le contenu ne peut etre vide" ]
+    }
+}, {
+    timestamps: true
+})
+
+const Tweet = mongoose.model('tweet', tweetSchema)
+
+
+module.exports = Tweet;
