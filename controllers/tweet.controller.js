cd907aca606ae1f5452df04a5ffe1d354e0cac32
Yuliia Kolomiiets <jk182013@gmail.com>
2023-03-01 10:10:59 +0100

    feat: Lier le tweet a l'auteur

--- controllers/tweet.controller.js
+++ controllers/tweet.controller.js
@@ -4,12 +4,12 @@ const { createNewTweet, findAllTweets, findTweetById, findTweetAndDelete, findTw
 exports.createTweet = async (req, res, next) => { 
     try {
         const body = req.body;
-        await createNewTweet(body);
+        await createNewTweet({...body, author: req.user._id});
         res.redirect('/')
     } catch (err) {
         const errors = Object.keys(err.errors).map(key => err.errors[key].message);
         const tweets = await findAllTweets()
-        res.status(400).render('tweets/tweet-list', {errors, tweets})
+        res.status(400).render('tweets/tweet-list', {errors, tweets, isAuthenticated: req.isAuthenticated(), currentUser: req.user})
     }
 }
 
@@ -30,7 +30,7 @@ exports.displayTweet = async (req, res, next) => {
     try {
         const tweetId = req.params.tweetId;
         const tweet = await findTweetById(tweetId);
-        res.render('tweets/tweet-edit', { tweet, isAuthenticated: req.isAuthenticated, currentUser: req.user });
+        res.render('tweets/tweet-edit', { tweet, isAuthenticated: req.isAuthenticated(), currentUser: req.user });
     } catch (error) {
         next(error)
     }
--- database/models/Tweet.model.js
+++ database/models/Tweet.model.js
@@ -8,6 +8,11 @@ const tweetSchema = schema({
         maxlength: [146, 'Le tweet est trop long'],
         minlength: [5, 'Le tweet est trop court'],
         requires: [ true, "Le contenu ne peut etre vide" ]
+    },
+    author: {
+        type: schema.Types.ObjectId,
+        ref: 'user', //referance a model User
+        required: true
     }
 }, {
     timestamps: true
