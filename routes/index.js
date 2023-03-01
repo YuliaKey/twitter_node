d7ee0e80dd8228cef29c45fb0db909015f414ef3
Yuliia Kolomiiets <jk182013@gmail.com>
2023-03-01 10:32:56 +0100

    feat: Middleware ensureAuthenticated

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ const tweetRoute = require('./tweet.routes')
 const userRoute = require('./user.routes')
 const authRoute = require('./auth.routes')
 const { tweetList } = require("../controllers/tweet.controller")
+const { ensureAuthenticated } = require("../config/security.config")
 
-router.use('/tweet', tweetRoute)
+router.use('/tweet', ensureAuthenticated, tweetRoute)
 router.use('/user', userRoute)
 router.use('/auth', authRoute)
 
